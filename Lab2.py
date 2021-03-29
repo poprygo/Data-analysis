91a34149feb2ca62b417684acd8bac33de3c9157
poprygo <70857282+poprygo@users.noreply.github.com>
2021-03-29 19:00:08 +0300

    Add files via upload

---
+++ Lab2.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = pd.read_json(r'output.json')
+# necessary columns
+fields = ['title', 'link', 'summary', 'published']
+
+# apply to_datetime function along published axis in data dataframe
+data['published'] = pd.to_datetime(data['published'])
+
+# output the result on all entries on necessary columns to output_task2.json file
+with open("output_Lab2.json", 'w') as outfile:
+    data[fields].to_json(outfile, orient="records")
+
+print(data[fields])
